e9fc59964a94e6b0dc7bc69c75b2c9a7ff09e3f4
nanderstabel <nanderstabel@gmail.com>
2020-12-12 23:22:21 +0100

    It is a start

---
+++ computor.py
@@ -0,0 +1,18 @@
+import sys
+import re
+
+class Computor:
+	def __init__(self, polynomial):
+		self.polynomial = polynomial
+	
+	def parse(self, polynomial):
+		print(polynomial.split())
+
+
+
+
+
+computor = Computor(sys.argv[1])
+
+
+i
