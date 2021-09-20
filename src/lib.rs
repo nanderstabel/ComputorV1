961d8cc294c86a095896177f713861c45d2ade7f
nstabel <n.g.stabel@hotmail.com>
2021-09-20 19:08:20 +0200

    Add first AST draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::iter::Peekable;
-use std::str::Chars;
+// use std::fmt;
 use Token::*;
 
 #[derive(Debug, Clone)]
@@ -12,65 +12,80 @@ pub enum Token {
 
 #[derive(Debug)]
 pub struct Node<'a> {
-	token: &'a Token,
+	token: Option<&'a Token>,
 	children: Vec<Node<'a>>
 }
 
-// #[derive(Debug, Default)]
+#[derive(Debug, Default)]
 pub struct Computor {
-    // buf: String,
-    tokens: Vec<Token>,
-	lexer: Box<Iterator<Item = char>>
+    buf: String,
 }
 
-// impl<'a> Default for Computor {
-// 	fn Default 
-// }
-
-impl Computor {
-    pub fn ingest(&mut self, buf: String) {
-        // self.buf = buf;
-		self.lexer = Box::new(buf.chars().peekable());
+impl<'a> Computor {
+    pub fn ingest(&mut self, buf: &str) {
+        self.buf = buf.to_string();
     }
 
-    // pub fn tokenize(&mut self) {
-    //     let mut lexer = self.buf.chars().peekable();
-    //     while let Some(c) = lexer.next() {
-    //         match c {
-    //             '+' | '-' | '*' | '/' | '^' | '=' => self.tokens.push(Operator(c)),
-	// 			'a' ..= 'z' | 'A' ..= 'Z' => self.tokens.push(Identifier(get_identifier(&mut lexer, c))),
-    //             '0' ..= '9' => self.tokens.push(Number(get_number(&mut lexer, c))),
-    //             '(' | ')' => self.tokens.push(Paren(c)),
-	// 			c if c.is_whitespace() => {},
-    //             _ => panic!("Unexpected char: {}", c)
-    //         }
-    //     }
-    // }
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut lexer = self.buf.chars().peekable();
+		let mut tokens: Vec<Token> = vec![];
+        while let Some(c) = lexer.next() {
+            match c {
+                '+' | '-' | '*' | '/' | '^' | '=' => tokens.push(Operator(c)),
+				'a' ..= 'z' | 'A' ..= 'Z' => tokens.push(Identifier(get_identifier(&mut lexer, c))),
+                '0' ..= '9' => tokens.push(Number(get_number(&mut lexer, c))),
+                '(' | ')' => tokens.push(Paren(c)),
+				c if c.is_whitespace() => {},
+                _ => panic!("Unexpected char: {}", c)
+            }
+        }
+		tokens
+    }
 
-	// pub fn parse(&mut self) {
-	// 	let mut tokens = self.tokens.iter().peekable();
-	// 	let mut tree : Vec<Node> = Vec::new();
-	// 	while let Some(token) = tokens.next() {
-	// 		match token {
-	// 			Number(num) => println!("number"),
-	// 			Operator('+') | Operator('-') => println!("operator"),
-	// 			_ => ()
-	// 		};
+	fn parse_recursive<I>(&mut self, tokens: &mut Peekable<I>) -> Option<Node<'a>>
+	where I: Iterator<Item = &'a Token> {
+		if let Some(token) = tokens.next() {
+			let mut new_node = Node {
+				token: Some(token),
+				children: vec![]
+			};
+			match token {
+				Token::Operator(op) => println!("{:?}, {}", new_node, op),
+				Token::Number(num) => println!("{:?}, {}", new_node, num),
+				Token::Identifier(var) => println!("{:?}, {}", new_node, var),
+				_ => ()
+			}
+			if let Some(child) = self.parse_recursive(tokens) {
+				new_node.children.push(child);
+			}
+			return Some(new_node);
+		}
+		None
+	}
 
-	// 		tree.push( Node {
-	// 			token: token,
-	// 			children: Vec::new()
-	// 		});
-
-	// 	}
-	// 	println!("{:#?}", tree);
-	// }
+	pub fn parse(&mut self) {
+		let tokens = self.tokenize();
+		let mut tree = Node {
+			token: None,
+			children: vec![]
+		};
+		if let Some(child) = self.parse_recursive(&mut tokens.iter().peekable()) {
+			tree.children.push(child);
+		}
+		println!("{:#?}", tree);
+	}
 
     // pub fn print(&mut self) {
     //     // println!("{}", self.buf);
     //     println!("{:?}", self.tokens);
     // }
 }
+
+// impl fmt::Display for Computor {
+// 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+// 		write!(f, )
+// 	}
+// }
 
 fn get_number<I>(lexer: &mut Peekable<I>, c: char) -> f64
 where I: Iterator<Item = char>, {
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
 
 
-
-
-
-
-
-
 // https://petermalmgren.com/three-rust-parsers/
 // https://mail.google.com/mail/u/0/#inbox/KtbxLthqCqfmTnHQpDJCWDbpmMKpWVvgsq?projector=1
 // 92
-
+// https://app.diagrams.net/#G14rzgULr5arR4jENATQecRsNx08YcXdhu
 
 use computorv1::Computor;
 use std::env;
 
 fn main () {
-    let mut computor = Computor::new(
-        lexer: Box::new()
-    );
+    let mut computor = Computor::default();
 
 
-    for arg in &env::args().skip(1) {
+    for arg in env::args().skip(1) {
         computor.ingest(&arg);
-        // computor.tokenize();
-		// computor.parse();
+		computor.parse();
         // computor.print();
     }
 
